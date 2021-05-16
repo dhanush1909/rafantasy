0d9819697b3e2f84d6004fa1496ec7a18437f04b
Acyuta <56015756+acyuta1@users.noreply.github.com>
2021-05-16 20:59:56 +0530

    added docker-support and applied image logic removal

--- liquibase/src/main/java/com/rafantasy/liquibase/LiquibaseApplication.java
+++ liquibase/src/main/java/com/rafantasy/liquibase/LiquibaseApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 @ComponentScan(basePackages = {""})
 public class LiquibaseApplication {
     public static void main(String[] args) {
