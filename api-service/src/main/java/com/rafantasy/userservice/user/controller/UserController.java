1b581e7b34c561d6017a857b297048930d71ce8a
Acyuta <56015756+acyuta1@users.noreply.github.com>
2021-06-18 23:52:44 +0530

    added shared module, added shared dependency in other modules

--- api-service/src/main/java/com/rafantasy/userservice/user/controller/UserController.java
+++ api-service/src/main/java/com/rafantasy/userservice/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.rafantasy.userservice.user.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import static com.rafantasy.shared.common.Constants.POC_APP_NO_REPLY; // example
 
 /**
  * User Controller.
@@ -17,5 +18,6 @@ public class UserController {
     public String getAllUsers() {
         return "works";
     }
+
 
 }
